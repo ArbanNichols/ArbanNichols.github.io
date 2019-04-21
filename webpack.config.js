59e0195b4082310d6e7aeeabe47b5b166ae1f6e7
code_shoulder <arban.nichs@gmail.com>
2019-04-20 23:53:47 -0400

    Code Submittal

--- webpack.config.js
+++ webpack.config.js
@@ -9,48 +9,58 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const APP_DIR = path.resolve(__dirname, './src/');
 
 module.exports = env => {
-    const { PLATFORM } = env;
-    return merge([
-        {
-            entry: ['@babel/polyfill', APP_DIR],
-            module: {
-                rules: [
-                    {
-                        test: /\.(js|jsx)$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: 'babel-loader',
-                        },
-                    },
-                    {
-                        test: /\.(scss|css)$/,
-                        use: [
-                            PLATFORM === 'production'
-                                ? MiniCssExtractPlugin.loader
-                                : 'style-loader',
-                            'css-loader',
-                            'sass-loader',
-                        ],
-                    },
-                ],
+  const { PLATFORM } = env;
+  return merge([
+    {
+      entry: ['@babel/polyfill', APP_DIR],
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
             },
-            plugins: [
-                new CopyWebpackPlugin([{ from: 'src/static' }]),
-                new HtmlWebpackPlugin({
-                    template: './src/index.html',
-                    filename: './index.html',
-                }),
-                new webpack.DefinePlugin({
-                    'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
-                }),
-                new webpack.ProvidePlugin({
-                    PropTypes: 'prop-types',
-                    axios: 'axios',
-                }),
+          },
+          {
+            test: /\.(scss|css)$/,
+            use: [
+              PLATFORM === 'production'
+                ? MiniCssExtractPlugin.loader
+                : 'style-loader',
+              'css-loader',
+              'sass-loader',
             ],
-            resolve: {
-                extensions: ['.js', '.jsx'],
+          },
+          {
+            test: /\.(ico|jpg)$/,
+            use: [{ loader: 'file-loader', options: {} }],
+          },
+          {
+            test: /\.svg/,
+            use: {
+              loader: 'svg-url-loader',
             },
-        },
-    ]);
+          },
+        ],
+      },
+      plugins: [
+        new CopyWebpackPlugin([{ from: 'src/static' }]),
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: './index.html',
+          favicon: './src/static/favicon.ico',
+        }),
+        new webpack.DefinePlugin({
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
+        }),
+        new webpack.ProvidePlugin({
+          PropTypes: 'prop-types',
+        }),
+      ],
+      resolve: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  ]);
 };
