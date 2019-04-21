69da88ee409c8843d1b30808d7554d7b6e617b0a
code_shoulder <arban.nichs@gmail.com>
2019-04-20 23:43:33 -0400

    Code submittal

---
+++ src/jsx/components/common/CoverLetter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const CoverLetter = () => {
+  return (
+    <div className="coverletter">
+      <h1>whoami</h1>
+      <strong>TLDR; </strong>
+      <span>
+        Veteran Air Force Electrician retrained to provide you websites and web
+        applications.
+      </span>
+      <p>
+        Nice to make your acquaintance! I am a former aircraft environmental and
+        electrical tehcnicain for the Air Force who used the GI Bill to gain a
+        B.Sc. in Computer Science. During my undergraduate studies I performed
+        research under network science study grants aimed at creating Android
+        applications optimized for delay laden networks. After my graduation
+        from York College in 2016, I worked as an embedded software engineer on
+        RTOS such as INTEGRITY and VxWorks. Afterwards I found enjoyment and
+        passion in creating UI using React.Js and web servers with NodeJs. Wow,
+        so early into my career but I have used so many different technologies!
+        Currently, I am continuing down the road of web development and
+        engineering so as to become a strong full-stack software engineer.
+      </p>
+    </div>
+  );
+};
+
+export default CoverLetter;
